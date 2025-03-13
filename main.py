58879302fd6615c117a3d8f64e022032c434d905
stichj <jonas.stich@tum.de>
2025-03-13 14:17:37 +0100

    Add bullets, shootinng functionality to game

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+from shot import Shot
 
 black = (0, 0, 0)
 
@@ -11,12 +16,52 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    Shot.containers = (shots, updatable, drawable)
+    
+    asteroidfield = AsteroidField()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
+        #player.update(dt)
+        #player.draw(screen)
+        
+        for object in updatable:
+            object.update(dt)
+        
+        for asteroid in asteroids:
+            if player.is_colliding(asteroid):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.is_colliding(shot):
+                    shot.kill()
+                    asteroid.split()
+        
+        for object in drawable:
+            object.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
     
 if __name__ == "__main__":
     main()
